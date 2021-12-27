5d1eec5c21b0c89e43483c77b571a3fd19c138b3
Mubashir <mubashirshoukat@gmail.com>
2021-12-27 16:14:59 +0300

    removed werk.zueg  module from app.py

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
-from flask import Flask, flash, redirect, render_template, request, session
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from flask import Flask, redirect, render_template, request
 
 # Configure application
 app = Flask(__name__)
-
-# Ensure responses aren't cached
-@app.after_request
-def after_request(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
 
 @app.route("/")
 def index():
